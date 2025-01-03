c75686efd4b744a3815201c6b110c5628b66dcb7
Jérémie <jeremieemk@gmail.com>
2025-01-03 08:12:05 +0100

    added tailwind styling

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+        './src/**/*.{html,js,jsx,ts,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                red: '#ff0000',
+                blue: '#0000ff',
+                yellow: '#ffff00',
+                pink: '#ff69b4',
+            },
+        },
+    },
+    plugins: [],
+}
